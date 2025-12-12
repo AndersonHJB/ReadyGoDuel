5c11964174b2eaa844ee5ac7a87bf5cc191727d7
AndersonHJB <bornforthis@bornforthis.cn>
2025-12-12 17:25:03 +0800

    更新

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env sh
+git pull
+git status
+git add .
+git commit . -m "update"
+git push -u origin main
+npm run build
+
+set -e
+cd dist
+echo "已成功进入目录打包...正在进行打包"
+zip -r ../zip/dist.zip ./
+echo "已经成功打包"
+
+echo "***** 上传中 *****"
+scp -v -i ~/.ssh/id_rsa -r ../zip/dist.zip root@121.89.218.11:/www/wwwroot/gemini.bornforthis.cn
+echo "***** 成功上传 *****"
+rm -rf ../zip/dist.zip
+echo "***** 进入服务器，触发远端程序 *****"
+# ssh root@121.89.218.11 "sh /bash/autounzip.sh"
+ssh -i ~/.ssh/id_rsa root@121.89.218.11 "sh /www/wwwroot/bash/gemini.sh"
+echo "***** 传输完毕*****"
+
+
+npm run dev
